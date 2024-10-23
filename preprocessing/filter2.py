457348a82e1e362b244cf3c94d9eaed5ecfe8302
LoganEricksen <lerickse@gmu.edu>
2024-10-23 16:16:55 -0400

    filter2 class made, which works the same as the first one but preprocesses the text for bert

--- preprocessing/filter2.py
+++ preprocessing/filter2.py
@@ -1,27 +1,19 @@
-
-
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from nltk  import sent_tokenize, word_tokenize 
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-
+from transformers import BertTokenizer, BertForMaskedLM
 import csv
 
 #methods used in this are slower but more accurate. Can be chaged if needed pretty easily at point
 #example, lemmination is slower then stemming, and nltk is slower than SpaCy
-class filter:
+class filter2:
     #returns a dictionary of twitter usernames mapped to a 2d array of a preprocessed tweet
     #plan, make dict, map usernames to a 2d array, 2d array is the processed tokens of each tweet that the username has made   
     def preprocess(filename):
         user_dict = dict()
         f = open(filename)
-        lem = WordNetLemmatizer()
 
         #o = open("output.txt", 'w')
-        analyzer = SentimentIntensityAnalyzer()
         with f as file_obj: 
             reader_obj = csv.reader(file_obj) 
-            stop_words = set(stopwords.words('english'))
+            tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
             total = 0
             for row in reader_obj:
                 #if statement just to control how much we do, this is a slow process. 
@@ -33,15 +25,13 @@ class filter:
                 if row[1] in user_dict: #if the item already exist, change the 2d array to the existing one that it points to
                     tweets = user_dict.get(row[1])
 
-                for s in word_tokenize(row[3]):
-                    l = lem.lemmatize(s)
-                    #this way of handling stop words is for now. need to look into what stopwords to keep and what to get rid of
-                    #and from there modify this accordingly 
-                    if s not in stop_words:
-                        lemd.append(l)
+                tokens = tokenizer.tokenize(row[3]) #converts the sentence into subwords
+                lemd = tokenizer.convert_tokens_to_ids(tokens) #converts the subword into numbers that bert understands
+                
+                
                 tweets.append(lemd)
                 user_dict.update({row[1] : tweets})
                 total+=1
         return user_dict
 
-print(filter.preprocess("../mock_datasets/twitter_training.csv").get("Borderlands"))
+print(filter2.preprocess("../mock_datasets/twitter_training.csv").get("Borderlands"))
