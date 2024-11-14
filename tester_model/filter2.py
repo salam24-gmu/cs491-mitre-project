c1ba3d5b4a44ad836ab9b8df799de724b639ea49
LoganEricksen <lerickse@gmu.edu>
2024-11-14 10:26:41 -0500

    basic sentiment scores implimented

--- tester_model/filter2.py
+++ tester_model/filter2.py
@@ -3,9 +3,9 @@ from transformers import  AutoTokenizer
 from transformers import BertTokenizer, BertForMaskedLM
 import csv
 import torch
-#from TweetNormalizer import normalizeTweet
-#methods used in this are slower but more accurate. Can be chaged if needed pretty easily at point
-#example, lemmination is slower then stemming, and nltk is slower than SpaCy
+from TweetNormalizer import normalizeTweet #useing a pre trained model for analyzing sentiment from tweets.
+#credit goes to https://github.com/VinAIResearch/BERTweet?tab=readme-ov-file#usage2
+#using the model to get values from tweets to train our own model on detecting insider threats
 class filter2:
     #returns a dictionary of twitter usernames mapped to a 2d array of a preprocessed tweet
     #plan, make dict, map usernames to a 2d array, 2d array is the processed tokens of each tweet that the username has made   
@@ -21,16 +21,14 @@ class filter2:
                 #if statement just to control how much we do, this is a slow process. 
                 if(total==100):
                     break
-                
-                lemd = [] #list of words after being lemmatized
                 tweets = [] #2d list
                 if row[id] in user_dict: #if the item already exist, change the 2d array to the existing one that it points to
                     tweets = user_dict.get(row[id])
 
-                #line = normalizeTweet(row[val])
-                tokens = torch.tensor([tokenizer.encode(row[val])]) #converts the sentence into subwords
-                print(tokens)
-                tweets.append(tokens)
+                line = normalizeTweet(row[val])
+                ids = torch.tensor([tokenizer.encode(line)])
+                #print(ids) 
+                tweets.append(ids)
                 user_dict.update({row[id] : tweets})
                 total+=1
         return user_dict
