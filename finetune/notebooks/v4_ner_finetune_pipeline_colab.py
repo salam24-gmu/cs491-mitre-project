c7c199efde8b10263aa6c0677461d0ff5efa9947
LoganEricksen <lerickse@gmu.edu>
2025-04-12 11:39:47 -0400

    should be able to run now locally

--- finetune/notebooks/v4_ner_finetune_pipeline_colab.py
+++ finetune/notebooks/v4_ner_finetune_pipeline_colab.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:353ab6cb3c7a1063f39d355db4e57456a0a9e682c37b96034a94ee3a5da81742
-size 26557
+oid sha256:736e54ab4a97bafb1d7642e3291b7f1f0ef51c03204d75613a0d298f7dd3127f
+size 26512
